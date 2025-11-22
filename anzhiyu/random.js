621039246392c33799f71bc02f788eea1fef5a47
fryfries13 <zenggaoyong1@outlook.com>
2025-11-22 09:49:48 +0800

    Site updated: 2025-11-22 09:49:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/MyAI/","post/EP07/","post/Ventoy也能豪堪？教你自定义Ventoy主题/"];function toRandomPost(){
+var posts=["post/Cloudflare 大规模中断事件（已修复）/","post/MyAI/","post/EP07/","post/Ventoy也能豪堪？教你自定义Ventoy主题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
